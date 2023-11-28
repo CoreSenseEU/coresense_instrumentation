168d5d387f04c59e0a2556bfc4df4047b5144e5a
Juancams <jc.manzanares.serrano@gmail.com>
2023-11-28 19:55:20 +0100

    Flake8 errors

--- coresense_instrumentation_driver/launch/coresense_instrumentation_driver.launch.py
+++ coresense_instrumentation_driver/launch/coresense_instrumentation_driver.launch.py
@@ -12,18 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
-
-    pkg_dir = get_package_share_directory('coresense_instrumentation_driver')
-    param_file = os.path.join(pkg_dir, 'config', 'params.yaml')
 
     coresense_node = ComposableNode(
         package='coresense_instrumentation_driver',
