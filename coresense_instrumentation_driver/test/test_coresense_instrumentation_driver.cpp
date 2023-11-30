34bb35a3c1f97dcfb8ec674e427d730d94626adc
Juancams <jc.manzanares.serrano@gmail.com>
2023-11-30 18:28:48 +0100

    Added new tests

--- coresense_instrumentation_driver/test/test_coresense_instrumentation_driver.cpp
+++ coresense_instrumentation_driver/test/test_coresense_instrumentation_driver.cpp
@@ -2,6 +2,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/string.hpp>
 #include "coresense_instrumentation_driver/InstrumentationLifecycleNode.hpp"
 
@@ -71,6 +72,48 @@ TEST_F(IntegrationTest, StringNodeLifecycle)
       "topic",
       "/test_topic").append_parameter_override(
       "topic_type", "std_msgs::msg::String"));
+
+  executor.add_node(node->get_node_base_interface());
+
+  auto result = node->on_configure(rclcpp_lifecycle::State());
+  ASSERT_EQ(
+    result,
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
+
+  result = node->on_activate(rclcpp_lifecycle::State());
+  ASSERT_EQ(
+    result,
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
+
+  node->on_activate(rclcpp_lifecycle::State());
+  result = node->on_deactivate(rclcpp_lifecycle::State());
+  ASSERT_EQ(
+    result,
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
+
+  result = node->on_cleanup(rclcpp_lifecycle::State());
+  ASSERT_EQ(
+    result,
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
+
+  result = node->on_shutdown(rclcpp_lifecycle::State());
+  ASSERT_EQ(
+    result,
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS);
+
+  executor.spin_once(std::chrono::seconds(1));
+}
+
+TEST_F(IntegrationTest, ImageNodeLifecycle)
+{
+  rclcpp::executors::SingleThreadedExecutor executor;
+
+  auto node =
+    std::make_shared<coresense_instrumentation_driver::InstrumentationLifecycleNode<std_msgs::msg::String>>(
+    rclcpp::NodeOptions().append_parameter_override(
+      "topic",
+      "/test_topic").append_parameter_override(
+      "topic_type", "sensor_msgs::msg::Image"));
 
   executor.add_node(node->get_node_base_interface());
 
